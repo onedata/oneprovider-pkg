a9794c010691dcced4b6a764a9428058364b3858
Łukasz Opioła <ymopiola@cyfronet.pl>
2023-05-29 18:25:34 +0200

    VFS-10612 Add scripts for graceful application stop and improve logging

--- docker/oneprovider.sh
+++ docker/oneprovider.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
 
-CEPH_STOP_TIMEOUT=60
+source /root/internal-scripts/common.sh
 
-function stop_service {
-  echo -e "Stopping ${2:-$1}..."
-  service "$1" stop >/dev/null 2>&1
+# @TODO VFS-10947-make-sure-onezone-oneprovider-entrypoint-is-started-with-pid-1
+
+function on_termination_signal {
+    dispatch-log "Received a termination signal"
+    /root/internal-scripts/oneprovider-ensure-stopped.sh
+    dispatch-log "Main process exiting"
 }
 
-function wait_for_process() {
-  while pgrep "$1" >/dev/null; do
-    sleep 0.5;
-  done
-}
+trap on_termination_signal SIGHUP SIGINT SIGTERM
 
-function stop_with_timeout {
-  export -f wait_for_process # make function available in timeout invocation
+# make sure the graceful stop marker is not set; see common.sh
+rm -f ${GRACEFUL_STOP_LOCK_FILE}
 
-  if pgrep "$1" >/dev/null; then
-    echo -e "Stopping ${2:-$1}..."
-    pkill -TERM -e "$1"
+# must be done before dispatch-log, which writes to a persistent directory
+/root/persistence-dir.py --copy-missing-files
 
-    timeout $CEPH_STOP_TIMEOUT bash -c "wait_for_process $1"
-    pkill -KILL -e "$1"
-  fi
-}
-
-function stop_oneprovider {
-  echo -e "\nGracefully stopping oneprovider...\n"
-
-  stop_service op_panel
-  stop_service op_worker
-  stop_service cluster_manager
-  stop_service couchbase-server couchbase
-  stop_with_timeout ceph-osd "ceph_osd"
-  stop_with_timeout ceph-mgr "ceph_mgr"
-  stop_with_timeout ceph-mon "ceph_mon"
-
-  echo -e "\nAll services stopped. Exiting..."
-
-  exit 0
-}
-
-trap stop_oneprovider SIGHUP SIGINT SIGTERM
+dispatch-log "Main process starting" extra_linebreak_in_log_file
 
 /root/oneprovider.py &
 wait $!
